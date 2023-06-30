ff19dee56087baca144492d79aa295fef514b029
Pavlik1996 <miki2012miki@gmail.com>
2023-06-30 22:17:15 +0300

    update

--- sendForm.js
+++ sendForm.js
@@ -1,7 +1,5 @@
-//Можно конечно использовать axios или fetch
 //лучше всего для такого рода сайта, есть библиотека EmailJs
 // Для своего портфолио именно так и делал
-//проект который сейчас пишу, использую RTKquery (лучше я ничего не знаю ))))
 
 document.addEventListener('DOMContentLoaded', function () {
 	let form = document.querySelector('#send-form')
